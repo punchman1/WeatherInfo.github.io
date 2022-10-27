158e7d1ffa0df9ded8be2a4eda06e4b2f0cde936
aman <agraharia329@gmail.com>
2022-10-27 11:08:43 +0530

    WeatherInfo.github.io
    WeatherInfo.github.io first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+// create your own web server call your api 
+
+const http= require("http");
+const fs=require("fs")
+var requests = require ("requests");
+
+const homeFile = fs.readFileSync("home.html","utf-8");
+// console.log(homeFile);
+
+
+const replaceVal=(tempVal,orgVal)=>{
+     let temperature = tempVal.replace("{%tempval%}",orgVal.main.temp);
+      temperature =temperature.replace("{%tempmin%}",orgVal.main.temp_min);
+      temperature =temperature.replace("{%tempmax%}",orgVal.main.temp_max);
+      temperature = temperature.replace("{%location%}",orgVal.name);
+      temperature = temperature.replace("{%country%}",orgVal.sys.country);
+      temperature = temperature.replace("{%tempstatus%}",orgVal.weather[0].main);
+      return temperature;
+}
+
+const server = http.createServer((req,res)=>{
+ // npm init    
+// install request npm 
+// npm i requests
+
+     if(req.url=="/")
+     {
+        requests('https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=2bed6af06728f4fa001b33a4b335035a')
+.on('data', (chunk)=> { //data event in streams
+     const objdata=JSON.parse(chunk);
+     const arrData=[objdata];
+     // console.log(arrData);
+     const realTimeData=arrData.map(val => replaceVal(homeFile,val)).join("");
+     // console.log(arrData[0].main.temp);
+     res.write(realTimeData);
+     // console.log(realTimeData);
+})
+.on('end', (err)=> {
+  if (err) return console.log('connection closed due to errors', err);
+     res.end();
+//   console.log('end');
+});
+     }
+});
+
+server.listen(8000,"127.0.0.1");
